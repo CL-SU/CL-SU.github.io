78700af436badb6f1cffb4329ccb09d4780d5f99
CL-SU <57026369+CL-SU@users.noreply.github.com>
2021-04-19 16:19:27 +0800

    123

--- assets/js/linechart.js
+++ assets/js/linechart.js
@@ -21,6 +21,7 @@ function loaddata(time){
     })
     .then((data) => {	
         // console.log("api data",data.data) 
+		current = data
 		ar_predict = data["prediction"][0]["data"].slice(time,time+period);
 		linechart.data.datasets[1].data = ar_predict;
 		rf_predict = data["prediction"][1]["data"].slice(time,time+period);
@@ -61,7 +62,7 @@ var linechart = new Chart(document.getElementById("linechart"),
 			data: [40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10],
 			fill: false,
 		}, {
-			label: 'AR prediction',
+			label: 'ARX prediction',
 			backgroundColor: window.chartColors.purple,
 			borderColor: window.chartColors.purple,
 			data: [40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10],
@@ -73,18 +74,12 @@ var linechart = new Chart(document.getElementById("linechart"),
 			borderColor: window.chartColors.navy,
 			data: [10,40,20,35,25,50,10,70,40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10],
 		},{
-			label: 'ANN prediction',
+			label: 'ANN(1) prediction',
 			backgroundColor: "rgb(00, 125, 00)",
 			borderColor: "rgb(00, 125, 00)",
 			data: [40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10],
 			// data: setTimeout(ar_predict, 1000),
 			fill: false,
-		}, {
-			label: 'CNN prediction',
-			fill: false,
-			backgroundColor: "rgb(255, 205, 86)",
-			borderColor: "rgb(255, 205, 86)",
-			data: [10,40,20,35,25,50,10,70,40,20,35,25,50,10,70,10,40,20,35,25,50,10,70,10],
 		}]
 	},
 	options: {
